c5ccde502d5d94bc654022ba036edba48ecb9a89
Vincent NOCK <vincent.nock@gmail.com>
2016-02-05 16:07:30 +0100

    removing html for text

--- library/src/main/java/it/sephiroth/android/library/tooltip/Tooltip.java
+++ library/src/main/java/it/sephiroth/android/library/tooltip/Tooltip.java
@@ -774,7 +774,7 @@ public final class Tooltip {
             mView.setLayoutParams(params);
 
             mTextView = (TextView) mView.findViewById(android.R.id.text1);
-            mTextView.setText(Html.fromHtml((String) this.mText));
+            mTextView.setText((String) this.mText);
             if (mMaxWidth > -1) {
                 mTextView.setMaxWidth(mMaxWidth);
                 log(TAG, VERBOSE, "[%d] maxWidth: %d", mToolTipId, mMaxWidth);
